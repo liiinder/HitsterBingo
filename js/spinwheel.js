f8b5368cad02740375db4531052d60648fdfb0de
Kristoffer Linder <linderkristoffer@hotmail.com>
2025-06-13 17:49:49 +0200

    small changes to the wheel

--- js/spinwheel.js
+++ js/spinwheel.js
@@ -4,5 +4,5 @@ let value = Math.ceil(Math.random() * 3600);
 
 spinBtn.onclick = function(){
     wheel.style.transform = "rotate(" + value + "deg)";
-    value += Math.ceil(Math.random() * 3600);
+    value += Math.ceil(Math.random() * 360 * 6) + 720; // minst 2 / max 8 varv
 }
