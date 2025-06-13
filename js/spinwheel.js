7bc2be13103d6f71a19cea951196152bb6197c38
Kristoffer Linder <linderkristoffer@hotmail.com>
2025-06-13 18:26:58 +0200

    changed back to dvw/dvh

--- js/spinwheel.js
+++ js/spinwheel.js
@@ -6,3 +6,5 @@ spinBtn.onclick = function(){
     wheel.style.transform = "rotate(" + value + "deg)";
     value += Math.ceil(Math.random() * 360 * 6) + 720; // minst 2 / max 8 varv
 }
+
+// based on -> https://www.youtube.com/watch?v=F3-lK_-PQr0
